98dd0a570e4f1b862d4760d4db6fe7f91cef7396
PierdomenicoPacilio <piero.pacilio@gmail.com>
2025-03-01 00:05:13 +0100

    fix: second code fix

--- pari-e-dospari/js/app.js
+++ pari-e-dospari/js/app.js
@@ -5,42 +5,52 @@
 // (usando una funzione) Dichiariamo chi ha vinto.
 
 
-// dichiarazione delle variabili e input:
+// dichiarazione delle variabili:
 let userChoice = '';
 let userNum = 0;
+
+// funzione per capire se l'input della scelta "pari" o "dispari" è giusta:
 function isEvenOrOdd(word){
-    if(word.toLowerCase() === 'pari'){
-        return true;
-    } else if(word.toLowerCase() === 'dispari'){
-        return true;
-    } else{
-        return false;
-    };
+    word = word.toLowerCase();
+    return word === 'pari' || word === 'dispari';
 };
+
+// input della scelta "pari" o "dispari":
 while (!(isEvenOrOdd(userChoice))){
     userChoice = prompt(`Scegli una tra le seguenti opzioni:
 - pari
 - dispari`);
         if (!(isEvenOrOdd(userChoice))) alert('Riprova, inserisci "pari" oppure "dispari".');
 };
+
+// input del numero da 1 a 5:
 while (!(1 <= userNum && userNum <= 5)){
-    userNum = Math.round(prompt(`Ora inserisci un numero da 1 a 5:`));
+    userNum = parseInt(prompt(`Ora inserisci un numero da 1 a 5:`));
     if (!(1 <= userNum && userNum <= 5)) alert('Riprova, inserisci un numero intero da 1 a 5.');
 };
+
+// calcolo numero del computer e somma di numero user + numero computer:
 const computerNum = Math.floor(Math.random() * 5 + 1);
 const sumNum = userNum + computerNum;
+
+// dichiarazione nuove variabili:
+userChoice = userChoice.toLowerCase();
 let sumNumResult = '';
 let result = '';
+
+// calcolo del vincitore:
 if (sumNum % 2 === 0){
     sumNumResult = 'pari';
 } else{
     sumNumResult = 'dispari';
 };
-if (userChoice.toLocaleLowerCase() === sumNumResult){
+if (userChoice === sumNumResult){
     result = 'hai vinto';
 } else{
     result = 'hai perso';
 };
-console.log(`Hai scelto ${userChoice.toLocaleLowerCase()} e il numero ${userNum}.
+
+// output:
+console.log(`Hai scelto ${userChoice} e il numero ${userNum}.
 Il computer ha scelto il numero ${computerNum}.
 La somma dei due numeri è ${sumNum}, ${sumNumResult}: ${result}.`);
